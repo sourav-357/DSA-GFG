c5242809c1e73be7b4c21738e5b00ac80ae7b82a
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-08 07:28:50 +0530

    Sum of first n Natural Numabers

---
+++ P02_Mathematics/SumOfNaturalNumber.java
@@ -0,0 +1,10 @@
+// Given an integer n, your task is to compute the sum of all natural numbers from 1 to n (inclusive). If n is 0, the sum should be 0.
+
+package P02_Mathematics;
+
+public class SumOfNaturalNumber {
+    public static int findSum(int n) {
+        // code here
+        return ((n * (n + 1)) / 2);
+    }
+}
