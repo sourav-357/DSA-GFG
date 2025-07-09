6513856696d1cdb72e06cf64e1638b5d87e3fdd4
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-09 08:50:16 +0530

    largest prime factor of n

--- P02_Mathematics/MediumProgram6.java
+++ P02_Mathematics/MediumProgram6.java
@@ -1,5 +1,28 @@
+// Given a number n, your task is to find the largest prime factor of n.
+
 package P02_Mathematics;
 
 public class MediumProgram6 {
-    
+    static int largestPrimeFactor(int n) {
+
+        int maxPrime = -1;
+
+        while (n % 2 == 0) {
+            maxPrime = 2;
+            n /= 2;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                maxPrime = i;
+                n /= i;
+            }
+        }
+
+        if (n > 2) {
+            maxPrime = n;
+        }
+
+        return maxPrime;
+    }
 }
