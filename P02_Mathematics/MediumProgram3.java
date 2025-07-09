b16e22ad44255b1f64478b024a116003c9913d96
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-09 08:54:35 +0530

    n raised to the power of its own reverse

--- P02_Mathematics/MediumProgram3.java
+++ P02_Mathematics/MediumProgram3.java
@@ -1,5 +1,32 @@
+// Given a number n, find the value of n raised to the power of its own reverse.
+// Note: The result will always fit into a 32-bit signed integer.
+
 package P02_Mathematics;
 
 public class MediumProgram3 {
-    
+    public int reverseExponentiation(int n) {
+        int rev = reverse(n);
+        return power(n, rev);
+    }
+
+    private int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+        return rev;
+    }
+
+    private int power(int base, int exp) {
+        int result = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result *= base;
+            }
+            base *= base;
+            exp >>= 1;
+        }
+        return result;
+    }
 }
