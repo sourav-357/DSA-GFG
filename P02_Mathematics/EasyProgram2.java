5a814738de3f9ca0c4b655aa851b92a367a6486c
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-09 07:51:39 +0530

    perfect number

--- P02_Mathematics/EasyProgram2.java
+++ P02_Mathematics/EasyProgram2.java
@@ -1,5 +1,20 @@
+// Given a number n, check if the number is perfect or not. A number is said to be perfect if sum of all its factors excluding the number itself is equal to the number.
+
 package P02_Mathematics;
 
 public class EasyProgram2 {
-    
+    static boolean isPerfect(int n) {
+        int sum = 0;
+
+        // check all numbers from 1 to n-1
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i != n && i != 1) {
+                    sum += (n / i);
+                }
+            }
+        }
+        return (sum == n);
+    }
 }
