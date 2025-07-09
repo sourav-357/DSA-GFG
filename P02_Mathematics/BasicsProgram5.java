d9a4f1baa5aeefa9035cd88a3ba1fcfe58ebc12e
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-09 07:45:58 +0530

    sum of numbers

--- P02_Mathematics/BasicsProgram5.java
+++ P02_Mathematics/BasicsProgram5.java
@@ -1,5 +1,10 @@
+// Given two numbers a and b. Your task is to return the sum of a and b.
+
 package P02_Mathematics;
 
 public class BasicsProgram5 {
-    
+    static int addition(int a, int b) {
+        // code here
+        return a + b;
+    }
 }
