f178192bd250ec57a4eb5b0fc51af806e92e26e5
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-09 07:44:50 +0530

    Factorial

--- P02_Mathematics/BasicsProgram4.java
+++ P02_Mathematics/BasicsProgram4.java
@@ -1,5 +1,13 @@
+// Given a positive integer, n. Find the factorial of n.
+
 package P02_Mathematics;
 
 public class BasicsProgram4 {
-    
+    static int factorial(int n) {
+        // code here
+        if(n==0)
+        return 1;
+        return n* factorial(n-1);
+        
+    }
 }
