ada7fce08ea284d0e41a43cb1e9afcc0b8a4ad64
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-10 09:28:49 +0530

    reverse exponenets

--- P02_Mathematics/EasyProgram3.java
+++ P02_Mathematics/EasyProgram3.java
@@ -1,5 +1,29 @@
 package P02_Mathematics;
 
 public class EasyProgram3 {
-    
+    public int reverseExponentiation(int n) {
+        int rev = reverse(n);
+        return power(n, rev);
+    }
+
+    private int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+        return rev;
+    }
+
+    private int power(int base, int exp) {
+        int result = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result *= base;
+            }
+            base *= base;
+            exp >>= 1;
+        }
+        return result;
+    }
 }
