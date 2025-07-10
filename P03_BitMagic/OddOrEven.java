be1e527247fc1241de8c79dcdaf61cb21682a9f8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-10 21:20:34 +0530

    Odd or even checker

---
+++ P03_BitMagic/OddOrEven.java
@@ -0,0 +1,11 @@
+// Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+
+package P03_BitMagic;
+
+public class OddOrEven {
+
+    static boolean isEven(int n) {
+        // code here
+        return n % 2 == 0;
+    }
+}
