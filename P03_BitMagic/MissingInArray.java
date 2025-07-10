8d63e13f08ed72b1c044a07213f9ef4e9b88f1f3
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-10 21:24:29 +0530

    Updated the question

--- P03_BitMagic/MissingInArray.java
+++ P03_BitMagic/MissingInArray.java
@@ -1,3 +1,5 @@
+// You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
+
 package P03_BitMagic;
 
 public class MissingInArray {
