5fe435eec8ce1d3f6bebbb9859d10185c4e1e181
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-16 23:16:23 +0530

    Updated

--- P05_Array/UnionOf2SoretedArray.java
+++ P05_Array/UnionOf2SoretedArray.java
@@ -1,3 +1,4 @@
+package P05_Array;
 // Given two sorted arrays a[] and b[], where each array may contain duplicate elements , the task is to return the elements in the union of the two arrays in sorted order.
 
 // Union of two arrays can be defined as the set containing distinct common elements that are present in either of the arrays.
