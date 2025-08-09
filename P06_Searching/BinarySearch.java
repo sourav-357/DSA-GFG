cc6c734e2a82b0f65ab57feff87c0017c03c7a14
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-09 21:52:49 +0530

    Binary Search

---
+++ P06_Searching/BinarySearch.java
@@ -0,0 +1,28 @@
+package P06_Searching;
+
+public class BinarySearch {
+    
+}
+class Solution {
+    static int binarysearch(int arr[], int k) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                ans = mid;
+                high = mid - 1; // move left to find smaller index
+            } 
+            else if (arr[mid] > k) {
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
