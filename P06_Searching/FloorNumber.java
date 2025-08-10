fdfa7b2c92bd988790fccc011c5fc9fa35c0fdd9
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-10 22:09:20 +0530

    Floor of Occourance

---
+++ P06_Searching/FloorNumber.java
@@ -0,0 +1,23 @@
+package P06_Searching;
+
+public class FloorNumber {
+    
+}
+class Solution {
+    public int findFloor(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1; // store index of floor
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x) {
+                ans = mid; // possible floor
+                low = mid + 1; // try to find a bigger one
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
