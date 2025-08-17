0298a0ee68c08b833e5261ffc0a80ed589ba222e
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-17 16:17:09 +0530

    Find the number of times an array is rotated

---
+++ P06_Searching/FindKthRotation.java
@@ -0,0 +1,29 @@
+package P06_Searching;
+
+public class FindKthRotation {
+    
+}
+class Solution {
+    public int findKRotation(int arr[]) {
+        
+        int low = 0, high = arr.length - 1 ;
+
+        while (low <= high) {
+             
+            // smallest is at low
+            if (arr[low] <= arr[high])
+                return low ;
+
+            int mid = (low + high) / 2 ;
+
+            // Minimum is in the right half
+            if (arr[mid] > arr[high])
+                low = mid + 1 ;
+
+            // Minimum is in the left half (could be mid)
+            else
+                high = mid ;
+        }
+        return low;
+    }
+}
