8084633e61ac789f76d29657819c6f33d8cfa4b3
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-17 16:14:00 +0530

    Searching an element in almost sorted array

---
+++ P06_Searching/SearchInAnAlmostSortedArray.java
@@ -0,0 +1,29 @@
+package P06_Searching;
+
+public class SearchInAnAlmostSortedArray {
+    
+}
+// User function Template for Java
+class Solution {
+    public int findTarget(int arr[], int target) {
+        
+        int low = 0, high = arr.length-1, mid = 0;
+        
+        while (low <= high) {
+            mid = (low+high)/2;
+            
+            if(arr[mid] == target) {
+                return mid;
+            } else if (mid < high && arr[mid + 1] == target) {
+                return mid + 1;
+            } else if (mid > low && arr[mid - 1] == target) {
+                return mid - 1;
+            } else if (arr[mid] > target) {
+                high = mid - 2;
+            } else {
+                low = mid + 2;
+            }
+        }
+        return -1;
+    }
+}
