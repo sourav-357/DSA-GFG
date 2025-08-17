821959e9988255552a4ae458820f3fa0b1cc9f99
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-17 16:15:09 +0530

    Single among the Doubles in a Sorted array

---
+++ P06_Searching/SingleAmongDoublesInSortedArray.java
@@ -0,0 +1,31 @@
+package P06_Searching;
+
+public class SingleAmongDoublesInSortedArray {
+    
+}
+class Solution {
+    int single(int[] arr) {
+        
+        int lo = 0, hi = arr.length - 1;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            // Ensure mid is odd
+            if (mid % 2 == 1)
+                mid--;
+            
+            // single element must be on the right side
+            if (arr[mid] == arr[mid + 1]) {
+                lo = mid + 2;
+            } 
+          
+            // Else single element must be on the left  
+            else {
+                hi = mid;
+            }
+        }
+        
+        return arr[lo];  
+    }
+}
