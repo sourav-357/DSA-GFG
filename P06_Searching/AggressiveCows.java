3319f8fae98111c54108d7bbdee39abb2844f97f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-18 19:24:40 +0530

    Aggressive Cows

--- P06_Searching/AggressiveCows.java
+++ P06_Searching/AggressiveCows.java
@@ -1,3 +1,7 @@
+/*  
+You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.
+*/
+
 package P06_Searching;
 
 public class AggressiveCows {
