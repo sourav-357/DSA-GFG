7e4083a187a4f079dcce9e8c789fe3d510bb1e80
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-18 19:29:22 +0530

    Majority eleemnt in an array

---
+++ P06_Searching/MajorityElement.java
@@ -0,0 +1,11 @@
+/*
+Given an array arr[]. Find the majority element in the array. If no majority element exists, return -1.
+
+Note: A majority element in an array is an element that appears strictly more than arr.size()/2 times in the array.
+*/
+
+package P06_Searching;
+
+public class MajorityElement {
+    
+}
