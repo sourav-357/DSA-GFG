e157cb3c5fcca6b8cd94c3b39e3f17ee605f4956
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-21 22:21:40 +0530

    merge without any extra space

--- P07_Sorting/MergeWithoutExtraSpace.java
+++ P07_Sorting/MergeWithoutExtraSpace.java
@@ -1,3 +1,7 @@
+// Given two sorted arrays a[] and b[] of size n and m respectively, 
+// the task is to merge them in sorted order without using any extra space. 
+// Modify a[] so that it contains the first n elements and modify b[] so that it contains the last m elements.
+
 package P07_Sorting;
 
 import java.util.Arrays;
