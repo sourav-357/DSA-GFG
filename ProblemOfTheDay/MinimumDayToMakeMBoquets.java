a85415930c1fd6f034931d5706dec3e720ff8abf
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-24 09:19:57 +0530

    added the question

--- ProblemOfTheDay/MinimumDayToMakeMBoquets.java
+++ ProblemOfTheDay/MinimumDayToMakeMBoquets.java
@@ -1,3 +1,13 @@
+/*
+You have a row of flowers, where each flower blooms after a specific day. 
+The array arr[] represents the blooming schedule: arr[i] is the day the flower at position i will bloom. 
+To create a bouquet, you need to collect k adjacent bloomed flowers. 
+Each flower can only be used in one bouquet.
+
+Your goal is to find the minimum number of days required to make exactly m bouquets. 
+If it is not possible to make m bouquets with the given arrangement, return -1. 
+*/
+
 package ProblemOfTheDay;
 
 class MinimumDayToMakeMBoquets {
