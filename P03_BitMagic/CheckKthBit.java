77d511fdb1a2cc034d46ac3879d5f15c3bf09ba0
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-02 08:26:35 +0530

    updated the code structure

--- P03_BitMagic/CheckKthBit.java
+++ P03_BitMagic/CheckKthBit.java
@@ -8,4 +8,15 @@ public class CheckKthBit {
         // code here
         return (n & (1 << k)) != 0;
     }
+    public static void main(String[] args) {
+        int n = 5; // Binary: 101
+        int k = 2; // Check if the 2nd bit is set
+        boolean result = checkKthBit(n, k);
+        System.out.println("Is the " + k + "th bit of " + n + " set? " + result);
+        
+        n = 5; // Binary: 101
+        k = 1; // Check if the 1st bit is set
+        result = checkKthBit(n, k);
+        System.out.println("Is the " + k + "th bit of " + n + " set? " + result);
+    }
 }
